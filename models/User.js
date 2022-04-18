cbb13d7471b55842f9a94faaab85dd892ad71e19
Vinny D <vdurham@prideseals.com>
2022-04-18 16:14:58 -0400

    password hashed

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
@@ -34,6 +35,16 @@ User.init(
     }
   },
   {
+    hooks: {
+        async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+        },
+        async beforeUpdate(updatedUserData) {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+        }
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
