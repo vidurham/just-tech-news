99607f73608fcc26730eae795ce4a02194a34acd
Vinny D <vdurham@prideseals.com>
2022-04-25 14:23:47 -0400

    single-post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -88,12 +88,14 @@ router.post('/', (req, res) => {
 });
 
 router.put('/upvote', (req, res) => {
-    Post.upvote(req.body, { Vote, Comment, User })
-        .then(updatedVoteData => res.json(updatedVoteData))
-        .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
-        });
+    if (req.session) {    
+        Post.upvote({...req.body, user_id: req.session.user_id}, { Vote, Comment, User })
+            .then(updatedVoteData => res.json(updatedVoteData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    }
 });
 
 router.put('/:id', (req, res) => {
--- server.js
+++ server.js
@@ -24,12 +24,12 @@ app.use(session(sess));
 
 const hbs = exphbs.create({});
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
 
 app.use(routes);
 
